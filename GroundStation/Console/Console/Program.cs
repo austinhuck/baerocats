95da069385f802a02db8169c0c4b346c796cfcdc
Austin Huck <austin_huck@outlook.com>
2016-11-04 03:19:32 -0400

    First console test program

--- GroundStation/Console/Console/Program.cs
+++ GroundStation/Console/Console/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using XBee;
+using XBee.Frames;
+using XBee.Frames.AtCommands;
 
 namespace Console
 {
@@ -10,6 +10,27 @@ namespace Console
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Initializing on COM9");
+
+            XBeeController controller = new XBeeController("COM9", 9600);
+
+            controller.NodeDiscovered += (s, a) =>
+            {
+                System.Console.WriteLine("Discovered {0}", a.Name);
+
+                a.Node.SetInputOutputConfigurationAsync(InputOutputChannel.Channel2, InputOutputConfiguration.DigitalIn).Wait();
+                a.Node.SetInputOutputConfigurationAsync(InputOutputChannel.Channel3, InputOutputConfiguration.AnalogIn).Wait();
+
+                a.Node.SetChangeDetectionChannelsAsync(DigitalSampleChannels.Input2).Wait();
+
+                a.Node.SetSampleRateAsync(TimeSpan.FromSeconds(5)).Wait();
+
+                a.Node.SampleReceived += (node, sample) => System.Console.WriteLine("Sample recieved: {0}", sample);
+            };  
+
+            controller.OpenAsync().Wait();
+            controller.DiscoverNetworkAsync().Wait();
+
         }
     }
 }
